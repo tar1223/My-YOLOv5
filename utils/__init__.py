fd5709b8cd2d1d2e26b5303e8e5e99ce9b222d26
tar1223 <tar1223@naver.com>
2025-01-29 07:50:33 +0000

    Implementation of the emojis function

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,6 @@
-def emojis():
-    pass
+import platform
+
+
+# 운영체제가 Windows인 경우에는 이모지를 제거하고, 그 외에는 그대로 반환
+def emojis(str=''):
+    return str.encode().decode('ascii', 'ignore') if platform.system() == 'Windows' else str

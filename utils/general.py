edce9948f81f161158536268654b558edf391b52
tar1223 <tar1223@naver.com>
2025-01-29 07:24:54 +0000

    Implementation of the set_logging function

--- utils/general.py
+++ utils/general.py
@@ -1,13 +1,51 @@
 import inspect
 import logging
+import os
+import platform
 from pathlib import Path
 from typing import Optional
+
+from utils import emojis
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[1]
 
 LOGGING_NAME = 'yolov5'
+
+
+def set_logging(name=LOGGING_NAME, verbose=True):
+    rank = int(os.getenv('RANK', -1))
+    level = logging.INFO if verbose and rank in {-1, 0} else logging.ERROR
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            name: {
+                'format": "%(message)s'
+            }
+        },
+        'handlers': {
+            name: {
+                'class': 'logging.StreamHandler',
+                'formatter': name,
+                'level': level
+            }
+        },
+        'loggers': {
+            name: {
+                'level': level,
+                'handlers': [name],
+                'propagate': False
+            }
+        }
+    })
+
+
+set_logging(LOGGING_NAME)
 LOGGER = logging.getLogger(LOGGING_NAME)
+if platform.system() == 'Windows':
+    for fn in LOGGER.info, LOGGER.warning:
+        setattr(LOGGER, fn.__name__, lambda x: fn(emojis(x)))
 
 
 def print_args(args: Optional[dict] = None, show_file=True, show_func=False):
